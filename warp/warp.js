7a719ca55b40579de2c506fd97e156342b395def
Ruben Verster <37653127+RubenVerster@users.noreply.github.com>
2019-07-16 14:55:16 +0200

    Add files via upload

--- warp/warp.js
+++ warp/warp.js
@@ -1,5 +1,5 @@
 const settings =
-  '{"speed":2,"speedAdjFactor":0.5,"density":3,"shape":"circle","warpEffect":true,"warpEffectLength":9,"depthFade":true,"starSize":2,"backgroundColor":"hsl(240,33%,7%)","starColor":"lightblue"}';
+  '{"speed":12,"speedAdjFactor":0.5,"density":3,"shape":"circle","warpEffect":true,"warpEffectLength":10,"depthFade":true,"starSize":2,"backgroundColor":"hsl(240,33%,7%)","starColor":"lightblue"}';
 new WarpSpeed('warp', settings);
 
 window.addEventListener('resize', new WarpSpeed('warp', settings));
